6a82556389095fa02641a4f2d1ff84541776fe80
Duallwaver <andrelourenco.al@outlook.pt>
2021-08-24 12:24:38 +0100

    jhsdfch

--- 5. BLE APP Blink/Ble_app_blink/lib/utils/BLE_util.h
+++ 5. BLE APP Blink/Ble_app_blink/lib/utils/BLE_util.h
@@ -7,8 +7,10 @@
 #include <BLE2902.h>
 #include "util.h"
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+//#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define SERVICE_UUID "544b335f-aa69-4be8-aef4-ca32a9867832"
+//#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC_UUID "e8695a15-7f64-4432-89af-2e42054399bb"
 
 #define BLE_SEND_SIZE 14
 
